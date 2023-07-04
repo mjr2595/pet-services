6b3a7ae274451a2e43926c5fec14544e58f9c7dd
mjr2595 <michaeljohnraymond@gmail.com>
2023-07-04 13:16:53 -0500

    About Me section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,22 @@
+import { Container, Stack, Heading, Button, Flex, Text, Image, Box, SimpleGrid } from "@chakra-ui/react";
+import kendallandjessie from "../assets/kendallandjessie.jpeg";
+
 const About = () => {
-  return <div id="about">About</div>;
+  return (
+    <Container id="about" as={Stack} spacing={4} p={8} maxW={"3xl"} alignItems={"center"}>
+      <Heading fontSize={"3xl"}>About me</Heading>
+      <Box p={4}>
+        <SimpleGrid id="aboutGrid" columns={{ base: 1, md: 2 }} spacing={10} justifyItems={"center"}>
+          <Image src={kendallandjessie} alt="About Me" boxSize="300px" borderRadius="full" />
+          <Text fontSize="lg">
+            Hello! I'd love to meet your furbaby! Hello! I'd love to meet your furbaby! Hello! I'd love to meet your
+            furbaby! Hello! I'd love to meet your furbaby! Hello! I'd love to meet your furbaby! Hello! I'd love to meet
+            your furbaby!Hello! I'd love to meet your furbaby!
+          </Text>
+        </SimpleGrid>
+      </Box>
+    </Container>
+  );
 };
 
 export default About;
