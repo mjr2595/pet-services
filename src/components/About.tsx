2bb3ca046c2fb172c49741556cfba45df6d8f4aa
mjr2595 <michaeljohnraymond@gmail.com>
2023-07-04 23:31:03 -0500

    Contact form and updated about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import { Container, Stack, Heading, Button, Flex, Text, Image, Box, SimpleGrid } from "@chakra-ui/react";
+import { Container, Stack, Heading, Text, Image, Box, SimpleGrid } from "@chakra-ui/react";
 import kendallandjessie from "../assets/kendallandjessie.jpeg";
 
 const About = () => {
@@ -9,9 +9,10 @@ const About = () => {
         <SimpleGrid id="aboutGrid" columns={{ base: 1, md: 2 }} spacing={10} justifyItems={"center"}>
           <Image src={kendallandjessie} alt="About Me" boxSize="300px" borderRadius="full" />
           <Text fontSize="lg">
-            Hello! I'd love to meet your furbaby! Hello! I'd love to meet your furbaby! Hello! I'd love to meet your
-            furbaby! Hello! I'd love to meet your furbaby! Hello! I'd love to meet your furbaby! Hello! I'd love to meet
-            your furbaby!Hello! I'd love to meet your furbaby!
+            Hi there! My name is Kendall. I have a sweet Boston Terrier and an energetic Black Mouth Cur. I have
+            experience working at a Veterinary Hospital so I’m used to being around furry friends. I'm fond of all
+            breeds and sizes! Your dog (or cat!) will have an amazing time while being in safe hands. I’ll treat them
+            like my own 🐶😻
           </Text>
         </SimpleGrid>
       </Box>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,110 +1,82 @@
 import {
   Container,
-  Flex,
   Box,
   Heading,
   Text,
   Button,
   VStack,
-  Wrap,
-  WrapItem,
   FormControl,
   FormLabel,
   Input,
   InputGroup,
   InputLeftElement,
   Textarea,
+  Stack,
+  Center,
 } from "@chakra-ui/react";
-import { MdPhone, MdLocationOn, MdOutlineEmail } from "react-icons/md";
+import { MdPhone, MdOutlineEmail } from "react-icons/md";
 import { BsPerson } from "react-icons/bs";
 
 const Contact = () => {
-  return (
-    <Container id="contact" bg="#BCD9FE" maxW="full" mt={0} centerContent overflow="hidden">
-      <Flex>
-        <Box bg="#02054B" color="white" borderRadius="lg" m={{ sm: 4, md: 16, lg: 10 }} p={{ sm: 5, md: 5, lg: 16 }}>
-          <Box p={4}>
-            <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
-              <WrapItem>
-                <Box>
-                  <Heading>Contact</Heading>
-                  <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                    Please fill out the form to contact me, or leave me a voicemail.
-                  </Text>
-                  <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                    <VStack pl={0} spacing={3} alignItems="flex-start">
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}
-                      >
-                        (512) 815-2561‬
-                      </Button>
+  const phoneNumber = "+15128152561";
 
-                      <Button
-                        size="md"
-                        height="48px"
-                        width="200px"
-                        variant="ghost"
-                        color="#DCE2FF"
-                        _hover={{ border: "2px solid #1C6FEB" }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}
-                      >
-                        Greater Austin Area, TX
-                      </Button>
-                    </VStack>
-                  </Box>
-                </Box>
-              </WrapItem>
-              <WrapItem>
-                <Box bg="white" borderRadius="lg">
-                  <Box m={8} color="#0B0E3F">
-                    <VStack spacing={5}>
-                      <form action="https://formsubmit.co/michaeljohnraymond@gmail.com" method="POST">
-                        <FormControl id="name" isRequired>
-                          <FormLabel>Your Name</FormLabel>
-                          <InputGroup borderColor="#E0E1E7">
-                            <InputLeftElement pointerEvents="none" children={<BsPerson color="gray.800" />} />
-                            <Input type="text" size="md" name="humanName" />
-                          </InputGroup>
-                        </FormControl>
-                        <FormControl id="name" isRequired>
-                          <FormLabel>Email</FormLabel>
-                          <InputGroup borderColor="#E0E1E7">
-                            <InputLeftElement pointerEvents="none" children={<MdOutlineEmail color="gray.800" />} />
-                            <Input type="email" size="md" name="email" />
-                          </InputGroup>
-                        </FormControl>
-                        <FormControl id="name" isRequired>
-                          <FormLabel>Message</FormLabel>
-                          <Textarea
-                            borderColor="gray.300"
-                            _hover={{
-                              borderRadius: "gray.300",
-                            }}
-                            placeholder="Tell me a bit about yourself and your pets!"
-                            name="message"
-                          />
-                        </FormControl>
-                        <FormControl id="name" float="right">
-                          <Button type="submit" variant="solid" bg="#0D74FF" color="white" _hover={{}}>
-                            Send Message
-                          </Button>
-                        </FormControl>
-                      </form>
-                    </VStack>
-                  </Box>
-                </Box>
-              </WrapItem>
-            </Wrap>
+  return (
+    <>
+      <Container id="contact" bg="#BCD9FE" maxW="full" mt={0} p={12} centerContent overflow="hidden">
+        <Box bg="white" borderRadius="lg">
+          <Box m={8} color="#0B0E3F">
+            <Container maxW={"7xl"} py={3} as={Stack} spacing={12}>
+              <Stack spacing={0} align={"center"}>
+                <Heading>Contact</Heading>
+                <Text>Please fill out the form to contact me. Can't wait to meet your furbaby!</Text>
+              </Stack>
+            </Container>
+
+            <VStack py={3} as={"form"} action="https://formsubmit.co/michaeljohnraymond@gmail.com" method="POST">
+              <FormControl id="name" isRequired>
+                <FormLabel>Your Name</FormLabel>
+                <InputGroup borderColor="#E0E1E7">
+                  <InputLeftElement pointerEvents="none" children={<BsPerson color="gray.800" />} />
+                  <Input type="text" size="md" name="humanName" />
+                </InputGroup>
+              </FormControl>
+              <FormControl id="name" isRequired>
+                <FormLabel>Email</FormLabel>
+                <InputGroup borderColor="#E0E1E7">
+                  <InputLeftElement pointerEvents="none" children={<MdOutlineEmail color="gray.800" />} />
+                  <Input type="email" size="md" name="email" />
+                </InputGroup>
+              </FormControl>
+              <FormControl id="name" isRequired>
+                <FormLabel>Message</FormLabel>
+                <Textarea
+                  size={"sm"}
+                  borderColor="gray.300"
+                  _hover={{
+                    borderRadius: "gray.300",
+                  }}
+                  placeholder="Tell me a bit about yourself and your pets!"
+                  name="message"
+                />
+              </FormControl>
+              <FormControl id="name" float="right">
+                <Center pt={3}>
+                  <Button type="submit" variant="solid" bg="#FF65C3" color="white" _hover={{}}>
+                    Send Message
+                  </Button>
+                </Center>
+              </FormControl>
+              <Text pt={6}>Or call and leave a message</Text>
+              <a href={`tel:${phoneNumber}`}>
+                <Button rightIcon={<MdPhone />} colorScheme="blue" variant="outline">
+                  Call us
+                </Button>
+              </a>
+            </VStack>
           </Box>
         </Box>
-      </Flex>
-    </Container>
+      </Container>
+    </>
   );
 };
 
