a7b23f460aaf07e5d5304113ad31020379b76166
mjr2595 <michaeljohnraymond@gmail.com>
2023-07-05 01:23:16 -0500

    Testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react";
-import { Box, Flex, Heading, Text, Stack, Container, Avatar, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Stack, Container, useColorModeValue, Image } from "@chakra-ui/react";
+import p1 from "../assets/pet-pics/dogandcat.png";
+import p2 from "../assets/pet-pics/jaxx.png";
+import p3 from "../assets/pet-pics/rubyandcooper.png";
+import p4 from "../assets/pet-pics/whitedogs.png";
 
 const Testimonial = ({ children }: { children: ReactNode }) => {
   return <Box>{children}</Box>;
@@ -36,17 +40,9 @@ const TestimonialContent = ({ children }: { children: ReactNode }) => {
   );
 };
 
-const TestimonialHeading = ({ children }: { children: ReactNode }) => {
-  return (
-    <Heading as={"h3"} fontSize={"xl"}>
-      {children}
-    </Heading>
-  );
-};
-
 const TestimonialText = ({ children }: { children: ReactNode }) => {
   return (
-    <Text textAlign={"center"} color={useColorModeValue("gray.600", "gray.400")} fontSize={"sm"}>
+    <Text textAlign={"center"} color={useColorModeValue("gray.600", "gray.400")} fontSize={"md"}>
       {children}
     </Text>
   );
@@ -55,7 +51,7 @@ const TestimonialText = ({ children }: { children: ReactNode }) => {
 const TestimonialAvatar = ({ src, name, title }: { src: string; name: string; title: string }) => {
   return (
     <Flex align={"center"} mt={8} direction={"column"}>
-      <Avatar src={src} mb={2} />
+      <Image src={src} mb={2} borderRadius={"lg"} boxSize="100px" objectFit="cover" />
       <Stack spacing={-1} align={"center"}>
         <Text fontWeight={600}>{name}</Text>
         <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
@@ -71,57 +67,40 @@ const Testimonials = () => {
     <Box id="testimonials" bg={useColorModeValue("gray.100", "gray.700")}>
       <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
         <Stack spacing={0} align={"center"}>
-          <Heading>Our Clients Speak</Heading>
-          <Text>We have been working with clients around the world</Text>
+          <Heading>Happy Pets, Happy People!</Heading>
+          <Text>Don't just take my word for it :)</Text>
         </Stack>
         <Stack direction={{ base: "column", md: "row" }} spacing={{ base: 10, md: 4, lg: 10 }}>
           <Testimonial>
             <TestimonialContent>
-              <TestimonialHeading>Efficient Collaborating</TestimonialHeading>
               <TestimonialText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat
-                nunc sem.
+                “Such a kind person and great caregiver! Went above and beyond for us and gave our pets lots of love!”
               </TestimonialText>
             </TestimonialContent>
-            <TestimonialAvatar
-              src={
-                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-              }
-              name={"Jane Cooper"}
-              title={"CEO at ABC Corporation"}
-            />
+            <TestimonialAvatar src={p1} name={"Human: Leah"} title={"Pets: Sturgill & Ginty"} />
           </Testimonial>
           <Testimonial>
             <TestimonialContent>
-              <TestimonialHeading>Intuitive Design</TestimonialHeading>
               <TestimonialText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat
-                nunc sem.
+                “I’ve tried other sitters but Kendall is the only person I trust with Jaxx”
               </TestimonialText>
             </TestimonialContent>
-            <TestimonialAvatar
-              src={
-                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-              }
-              name={"Jane Cooper"}
-              title={"CEO at ABC Corporation"}
-            />
+            <TestimonialAvatar src={p2} name={"Human: Georgia"} title={"Pet: Jaxx"} />
           </Testimonial>
           <Testimonial>
             <TestimonialContent>
-              <TestimonialHeading>Mindblowing Service</TestimonialHeading>
               <TestimonialText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor neque sed imperdiet nibh lectus feugiat
-                nunc sem.
+                “I said ‘Kendall’ and Cooper looked towards the front door, Ruby is a bit sad she definitely misses you!
+                Tells us that you're so good to them, thank you!”
               </TestimonialText>
             </TestimonialContent>
-            <TestimonialAvatar
-              src={
-                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-              }
-              name={"Jane Cooper"}
-              title={"CEO at ABC Corporation"}
-            />
+            <TestimonialAvatar src={p3} name={"Humans: Nick & Noelia"} title={"Pets: Ruby & Cooper"} />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialText>"Kendall is the best!"</TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar src={p4} name={"Human: Brennen"} title={"Pets: Casper & Winter"} />
           </Testimonial>
         </Stack>
       </Container>
